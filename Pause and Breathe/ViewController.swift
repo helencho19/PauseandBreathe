7e08346607f8448d71f0500979eaf4617647e7ec
Helen Cho <helencho2014@gmail.com>
2019-12-01 23:46:37 -0500

    Initial Commit

---
+++ Pause and Breathe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pause and Breathe
+//
+//  Created by Helen Cho on 12/1/19.
+//  Copyright © 2019 CS50. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
